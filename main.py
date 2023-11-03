63316135674c1947fc5d17d643f6efb8e41008cc
matveizhukov <motaigrok81@gmail.com>
2023-11-03 23:20:24 +0100

    stworzenie repoz

--- main.py
+++ main.py
@@ -2,14 +2,8 @@
 # Pobieranie długości boków od użytkownika
 dlugosc = float(input("Podaj długość boku a: "))
 szerokosc = float(input("Podaj długość boku b: "))
-
-# Obliczanie pola prostokąta
 pole = dlugosc * szerokosc
-
-# Obliczanie obwodu prostokąta
 obwod = 2 * (dlugosc + szerokosc)
-
-# Wyświetlanie wyników
 print(f"Pole prostokąta o bokach {dlugosc} i {szerokosc} wynosi {pole}")
 print(f"Obwód prostokąta o bokach {dlugosc} i {szerokosc} wynosi {obwod}")
 
