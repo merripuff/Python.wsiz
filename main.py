9ea9b23464b6a81a106a41429b5395eaecdc1334
matveizhukov <motaigrok81@gmail.com>
2023-11-05 16:08:43 +0100

    stworzenie repoz

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 #zad4.1
 import random
-
-# Generowanie losowej długości drogi (liczba całkowita z zakresu <1, 100000>)
 dlugosc_drogi = random.randint(1, 100000)
-
-# Średnie spalanie (wprowadź dowolne wartości)
 spalanie = float(input("Podaj średnie spalanie (w litrach na 100 km): "))
-
-# Cena paliwa
 cena_paliwa = 6.5  # Cena paliwa (zł/l)
-
-# Obliczanie zużycia paliwa
 zuzyte_paliwo = (dlugosc_drogi / 100) * spalanie
-
-# Obliczanie kosztów podróży
 koszty_podrozy = zuzyte_paliwo * cena_paliwa
-
-# Wyświetlanie wyników
 print(f"Przewidziane zużycie paliwa: {zuzyte_paliwo:.2f} litrów")
 print(f"Koszty podróży wyniosą: {koszty_podrozy:.2f} zł")
