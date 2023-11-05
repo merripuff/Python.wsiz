11f02432da9791b17cb36832fc71b8b471028d2f
matveizhukov <motaigrok81@gmail.com>
2023-11-05 17:11:52 +0100

    zadanie 4

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#zad3
-dlugosc = float(input("Podaj długość boku a: "))
-szerokosc = float(input("Podaj długość boku b: "))
-pole = dlugosc * szerokosc
-obwod = 2 * (dlugosc + szerokosc)
-print(f"Pole prostokąta o bokach {dlugosc} i {szerokosc} wynosi {pole}")
-print(f"Obwód prostokąta o bokach {dlugosc} i {szerokosc} wynosi {obwod}")
+#zad4
+dlugosc_drogi = float(input("Podaj długość drogi (w km): "))
+spalanie = float(input("Podaj średnie spalanie (w litrach na 100 km): "))
+cena_paliwa = 6.5
+zuzyte_paliwo = (dlugosc_drogi / 100) * spalanie
+koszty_podrozy = zuzyte_paliwo * cena_paliwa
+print(f"Przewidziane zużycie paliwa: {zuzyte_paliwo:.2f} litrów")
+print(f"Koszty podróży wyniosą: {koszty_podrozy:.2f} zł")
