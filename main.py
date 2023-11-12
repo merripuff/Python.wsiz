5c45afb304739275bd661aff9863195244d03afc
matveizhukov <motaigrok81@gmail.com>
2023-11-12 15:20:55 +0100

    zadanie 4 lab 2

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-#zad3
-while True:
-    liczba = int(input("Podaj liczbę całkowitą (liczba ujemna kończy pętlę): "))
-
-    if liczba < 0:
-        print("Wprowadzona liczba ujemna. Koniec pętli.")
-        break
+#zad4
+a = int(input("Podaj pierwszą liczbę całkowitą: "))
+b = int(input("Podaj drugą liczbę całkowitą: "))
+# Wybór początkowej i końcowej liczby do wypisania
+p = min(a, b)
+k = max(a, b)
+print("Liczby parzyste od {p} do {k}: ")
+for liczba in range(p, k + 1):
+    if liczba % 2 == 0:
+        print(liczba)
+    else:
+        continue
