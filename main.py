7be0b10156b94ea39d9059196c4e47c1a5d8cae9
matveizhukov <motaigrok81@gmail.com>
2023-11-12 14:45:49 +0100

    zadanie 4 lab 2

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-#zad3
-a = float(input("Podaj współczynnik a: "))
-b = float(input("Podaj współczynnik b: "))
-c = float(input("Podaj współczynnik c: "))
-delta = b**2 - 4*a*c
-print("Delta: ", delta)
-if a != 0:
-    if delta < 0:
-        print("Równanie nie ma rozwiązań")
-    elif delta > 0:
-            x1 = (-b + delta**(1/2)) / (2*a)
-            x2 = (-b - delta**(1/2)) / (2*a)
-            print("Równanie ma dwa rozwiązania: ", x1, x2)
-    else:
-        x = -c/b
-        print("Równanie ma jedno rozwiązanie: ", x)
-else:
-    x = -c/b
-    print("Równanie nie jest kwadratowe i ma dwa rozwiązanie: ", x)
+#zad4
+x = float(input("Podaj pierwszą liczbę (x): "))
+y = float(input("Podaj drugą liczbę (y): "))
+z = float(input("Podaj trzecią liczbę (z): "))
+if x > y:
+    x, y = y, x
+if y > z:
+    y, z = z, y
+if x > y:
+    x, y = y, x
+print("Liczby posortowane od najmniejszej do największej:")
+print(x, y, z)
