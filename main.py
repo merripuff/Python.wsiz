2b2d6bbbafd3c90ab12224206f69b111d6e0d80f
matveizhukov <motaigrok81@gmail.com>
2023-11-12 16:27:57 +0100

    zadanie 5 lab 2

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-#zad4
-a = int(input("Podaj pierwszą liczbę całkowitą: "))
-b = int(input("Podaj drugą liczbę całkowitą: "))
-# Wybór początkowej i końcowej liczby do wypisania
-p = min(a, b)
-k = max(a, b)
-print("Liczby parzyste od {p} do {k}: ")
-for liczba in range(p, k + 1):
-    if liczba % 2 == 0:
-        print(liczba)
-    else:
-        continue
+#zad5
+n = int(input("Podaj liczbę studentów w grupie: "))
+suma_punktow = 0
+licznik_studentow = 0
+while licznik_studentow < n:
+    punkty = float(input(f"Podaj liczbę punktów dla studenta {licznik_studentow + 1}: "))
+    suma_punktow += punkty
+    licznik_studentow += 1
+srednia = suma_punktow / n
+print("Średnia liczba punktów w grupie to: ", srednia )
