d20e555132dbbb672e0fc1137e8118be0dea8092
matveizhukov <motaigrok81@gmail.com>
2023-11-12 13:17:45 +0100

    zadanie 2 lab 2

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-#zad1
-wiek = int(input("Podaj wiek: "))
-if wiek < 0:
-    print("Niepoprawne dane")
-elif wiek <= 4:
-    print("Wejście za darmo")
-elif wiek <= 18:
-    print("Cena biletu: 10zł")
+#zad2
+def dodawanie(a, b):
+    return a + b
+def odejmowanie(a, b):
+    return a - b
+def mnozenie(a, b):
+    return a * b
+def dzielenie(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Błąd: Dzielenie przez zero"
+def potegowanie(a, b):
+    return a ** b
+print("Jaką operację chcesz wykonać?")
+print("1) dodawanie")
+print("2) odejmowanie")
+print("3) mnożenie")
+print("4) dzielenie")
+print("5) potęgowanie")
+operacja = int(input("Wpisz numer operacji: "))
+arg1 = float(input("Podaj argument 1: "))
+arg2 = float(input("Podaj argument 2: "))
+if operacja == 1:
+    wynik = dodawanie(arg1, arg2)
+elif operacja == 2:
+    wynik = odejmowanie(arg1, arg2)
+elif operacja == 3:
+    wynik = mnozenie(arg1, arg2)
+elif operacja == 4:
+    wynik = dzielenie(arg1, arg2)
+elif operacja == 5:
+    wynik = potegowanie(arg1, arg2)
 else:
-    print("Cena biletu: 20zł")
-
+    wynik = "Błąd: Nieprawidłowy numer operacji"
+print("Wynik:", wynik)
