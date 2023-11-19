1488ef5f548ed189f5c83d41deab4759a1bba2ff
matveizhukov <motaigrok81@gmail.com>
2023-11-19 22:22:44 +0100

    zadanie 5 lab 3

--- Lab 3.py
+++ Lab 3.py
@@ -1,8 +1,11 @@
-n=int(input("Który wyraz ciągu obliczyć: "))
-a=int(input("Podaj pierwszy wyraz ciągu: "))
-r=int(input("Podaj różnicę ciągu: "))
-for i in range(n):
-    x=a+i*r
-    print(x)
-
-
+number = int(input("Podaj liczbę naturalną: "))
+def s(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * s(n - 1)
+if number < 0:
+    print("Podana liczba jest mniejszą od zera. Silnia nie istnieje dla liczb ujemnych.")
+else:
+    result = s(number)
+    print(f"Silnia liczby {number} wynosi: {result}")
