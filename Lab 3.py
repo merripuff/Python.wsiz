648d5a3ce36a0c4251753a61873dda9e5426ac23
matveizhukov <motaigrok81@gmail.com>
2023-11-19 23:25:42 +0100

    zadanie 3 po lab 3

--- Lab 3.py
+++ Lab 3.py
@@ -1,8 +1,31 @@
-tekst = "Python jest super"
-print("Zerowy indeks:", tekst[0])
-print("Ostatni indeks:", tekst[-1])
-print("Co drugi, zaczynając od zerowego:", tekst[0:18:2])
-co_trzeci_pierwszy = tekst[1::3]
-print("Co trzeci zaczynając od pierwszego:", tekst[1:18:3])
-print("Od dziesiątego do ostatniego:", tekst[9:18])
-print("Od końca do początku:", tekst[::-1])
+name = input("Jak masz na imię? ")
+print(f"Witaj, {name}!")
+
+wiek = int(input("Podaj swój wiek: "))
+if wiek <= 0:
+     wiek = int(input("Podaj swój poprawny wiek: "))
+     print("Twój wiek to: ", wiek)
+elif wiek >= 0:
+    print("Twój wiek to: ", wiek)
+
+imie = input("Podaj swoje imię: ")
+nazwisko = input("Podaj swoje nazwisko: ")
+inicjaly = imie[0] + nazwisko[0]
+print("Twoje inicjały to:", inicjaly)
+
+lanuch = input("Podaj jakiś łańcuch: ")
+for i in range(5):
+    print(lanuch)
+
+first = input("Podaj pierwszy łańcuch: ")
+second = input("Podaj drugi łańcuch: ")
+third = first + second
+print("Połączone łańcuchy:", third)
+
+one = input("Podaj pierwszy łańcuch: ")
+two = input("Podaj drugi łańcuch: ")
+h1 = one[:len(one)//2]
+h2 = two[len(two)//2:]
+three = h1 + h2
+print("Połączone połowy:", three)
+
