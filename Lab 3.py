43a3c284718db4722252fcd30426098bb9422173
matveizhukov <motaigrok81@gmail.com>
2023-11-19 22:05:24 +0100

    zadanie 4 lab 3

--- Lab 3.py
+++ Lab 3.py
@@ -1,8 +1,8 @@
-a=int(input("Podaj wysokość choinki: "))
-for i in range(1, a + 1):
-    print("* " * i)
+n=int(input("Który wyraz ciągu obliczyć: "))
+a=int(input("Podaj pierwszy wyraz ciągu: "))
+r=int(input("Podaj różnicę ciągu: "))
+for i in range(n):
+    x=a+i*r
+    print(x)
 
-a=int(input("Podaj wysokość choinki: "))
-for i in range(1, a + 1):
-    print(" " * (a - i) + "* " * i)
 
