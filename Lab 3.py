ce411aaac42e5e0c2d1770d94c75531125a52a5e
matveizhukov <motaigrok81@gmail.com>
2023-11-19 22:34:41 +0100

    zadanie 6 lab 3

--- Lab 3.py
+++ Lab 3.py
@@ -2,7 +2,6 @@ dane = int(input("Wprowadź liczbę całkowitą: "))
 while True:
     if dane >= 0:
         print("To jest liczba")
-        continue
     else:
         print(f"Pierwiastek kwadratowy z {dane} wynosi: {0.5*dane}")
         print("Dziękujemy za skorzystanie z naszej aplikacji")
