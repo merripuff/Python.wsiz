d242653f8a26bf62968e4770e297143ab5fb667d
matveizhukov <motaigrok81@gmail.com>
2023-12-18 21:32:59 +0100

    zadanie dodatkowe 4 lab 4

--- Lab4.py
+++ Lab4.py
@@ -1,5 +1,10 @@
-zdanie = input("Podaj zdanie: ")
-wyrazy = zdanie.split()
-zdanie_sformatowane = ' '.join(word.capitalize() for word in wyrazy)
-print("Zdanie z każdym wyrazem zaczynającym się i kończącym wielką literą:")
-print(zdanie_sformatowane)
+ciag_znakow = input("Podaj ciąg znaków: ")
+licznik_znakow = {}
+ciag_zmieniony = ''
+for znak in ciag_znakow:
+    if znak in licznik_znakow:
+        ciag_zmieniony += '@'
+    else:
+        licznik_znakow[znak] = 1
+        ciag_zmieniony += znak
+print("Zmieniony ciąg znaków:", ciag_zmieniony)
