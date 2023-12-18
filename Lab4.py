f04a7bfe6edb985b43d9963312fe6f70cf4d35da
matveizhukov <motaigrok81@gmail.com>
2023-12-18 21:02:07 +0100

    zadanie 1 po lab 4

--- Lab4.py
+++ Lab4.py
@@ -1,16 +1,30 @@
-imię = input("Podaj swoje imię: ")
-print("Witaj, ", imię)
+def generuj_liste(n, x):
+    lista = ["".join(random.choice(string.ascii_lowercase) for _ in range(x)) for _ in range(n)]
+    return lista
+def konwertuj_na_krotke(lista):
+    return tuple(lista)
 
-wiek = input("Podaj swój wiek: ")
-print("Twój wiek to: ", wiek)
+import random
+import string
 
-nazwisko = input("Podaj nazwisko: ")
-print("Twoje iniciały: ", imię[0], nazwisko[0])
+n = int(input("Podaj wartość n: "))
+x = int(input("Podaj wartość x: "))
 
-łańcuch = input("Podaj łańcuch: ")
-łańcuchx5 = łańcuch*5
-print(łańcuchx5)
-drugi_łańcuch = input("Podaj drugi łańcuch: ")
-print(łańcuch, drugi_łańcuch)
+lista_ciagow = generuj_liste(n, x)
+
+krotka_ciagow = konwertuj_na_krotke(lista_ciagow)
+
+ilosc_znakow = sum(len(ciag) for ciag in lista_ciagow)
+print(f"a) Ilość znaków w liście: {ilosc_znakow}")
+
+ilosc_k = sum(ciag.count('k') for ciag in lista_ciagow)
+print(f"b) Ilość liter 'k' w liście: {ilosc_k}")
+
+ilosc_kt = sum(ciag.count('kt') for ciag in lista_ciagow)
+print(f"c) Ilość ciągów 'kt' w liście: {ilosc_kt}")
+
+s = int(input("Podaj wartość s dla sprawdzenia długości ciągów: "))
+ilosc_dluzszych_niz_s = sum(1 for ciag in lista_ciagow if len(ciag) > s)
+print(f"d) Ilość ciągów dłuższych niż {s}: {ilosc_dluzszych_niz_s}")
 
 
