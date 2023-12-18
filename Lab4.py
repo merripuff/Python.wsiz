2e8c7afdc81b639135a807effb9af281669aae54
matveizhukov <motaigrok81@gmail.com>
2023-12-18 21:16:34 +0100

    zadanie 4 po lab 4

--- Lab4.py
+++ Lab4.py
@@ -1,35 +1,45 @@
+import random
 
-rachunki_za_prad = {
-    'styczeń': 250,
-    'luty': 280,
-    'marzec': 220,
-    'kwiecień': 300,
-    'maj': 260,
-    'czerwiec': 240,
-    'lipiec': 280,
-    'sierpień': 310,
-    'wrzesień': 270,
-    'październik': 290,
-    'listopad': 250,
-    'grudzień': 300
-}
+a = random.randint(3, 7)
+b = random.randint(3, 7)
 
-lista_rachunkow = list(rachunki_za_prad.values())
+X = set(random.randint(0, 10) for _ in range(a))
+Y = set(random.randint(0, 10) for _ in range(b))
 
-maksymalna_wartosc = max(lista_rachunkow)
-minimalna_wartosc = min(lista_rachunkow)
-suma_wartosci = sum(lista_rachunkow)
-srednia_wartosc = suma_wartosci / len(lista_rachunkow)
+print(f"a) Zbiór X zawiera liczbę 5: {5 in X}")
 
-print(f"Maksymalna wartość: {maksymalna_wartosc}")
-print(f"Minimalna wartość: {minimalna_wartosc}")
-print(f"Suma wartości: {suma_wartosci}")
-print(f"Średnia wartość: {srednia_wartosc}")
+print(f"b) Zbiór X jest podzbiorem zbioru Y: {X.issubset(Y)}")
 
-ostatni_miesiac = list(rachunki_za_prad.keys())[-1]
-ostatni_rachunek = rachunki_za_prad[ostatni_miesiac]
+print(f"c) Zbiór Y jest podzbiorem zbioru X: {Y.issubset(X)}")
 
-if ostatni_rachunek > srednia_wartosc:
-    print("Zacznij oszczędzać.")
-else:
-    print("Jesteś bezpieczny.")
+print(f"d) Zbiór X jest nadzbiorem zbioru Y: {X.issuperset(Y)}")
+
+print(f"e) Zbiór Y jest nadzbiorem zbioru X: {Y.issuperset(X)}")
+
+suma_zbiorow = X.union(Y)
+print(f"f) Suma zbiorów X i Y: {suma_zbiorow}")
+
+roznica_X_Y = X.difference(Y)
+print(f"g) Różnica zbiorów X i Y: {roznica_X_Y}")
+
+roznica_Y_X = Y.difference(X)
+print(f"h) Różnica zbiorów Y i X: {roznica_Y_X}")
+
+iloczyn_zbiorow = X.intersection(Y)
+print(f"i) Iloczyn zbiorów X i Y: {iloczyn_zbiorow}")
+
+roznica_symetryczna = X.symmetric_difference(Y)
+print(f"j) Różnica symetryczna zbiorów X i Y: {roznica_symetryczna}")
+
+max_wartosc_X = max(X)
+max_wartosc_Y = max(Y)
+print(f"k) Najwyższa wartość w zbiorze X: {max_wartosc_X}")
+print(f"   Najwyższa wartość w zbiorze Y: {max_wartosc_Y}")
+
+pierwszy_element_X = X.pop()
+Y.add(pierwszy_element_X)
+
+Y.update(X.copy())
+
+X.clear()
+Y.clear()
