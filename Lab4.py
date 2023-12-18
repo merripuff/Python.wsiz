610ae5ba7b727e24b67d3686e71d81a28650492d
matveizhukov <motaigrok81@gmail.com>
2023-12-18 20:19:48 +0100

    zadanie 3 lab 4

--- Lab4.py
+++ Lab4.py
@@ -1,3 +1,6 @@
-tekst = "Rzeszów jest piękny"
-print("a)", tekst[0])
-print("b)", tekst[6:12:3], tekst[1])
+tekst ="Python test super"
+print("a)", tekst[0], tekst[-1])
+print("b)", tekst[0::2])
+print("c)", tekst[1::3])
+print("d)", tekst[10:])
+print("e)", tekst[::-1])
