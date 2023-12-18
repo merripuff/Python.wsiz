56b06d59d2d9e2bdcf33f48f858dbb59068ccef2
matveizhukov <motaigrok81@gmail.com>
2023-12-18 21:34:39 +0100

    zadanie dodatkowe 5 lab 4

--- Lab4.py
+++ Lab4.py
@@ -1,10 +1,8 @@
-ciag_znakow = input("Podaj ciąg znaków: ")
-licznik_znakow = {}
-ciag_zmieniony = ''
-for znak in ciag_znakow:
-    if znak in licznik_znakow:
-        ciag_zmieniony += '@'
-    else:
-        licznik_znakow[znak] = 1
-        ciag_zmieniony += znak
-print("Zmieniony ciąg znaków:", ciag_zmieniony)
+zdanie = input("Podaj zdanie: ")
+
+slowa = zdanie.split()
+
+najdluzsze_slowo = max(slowa, key=len)
+
+print("Najdłuższe słowo:", najdluzsze_slowo)
+print("Długość najdłuższego słowa:", len(najdluzsze_slowo))
