da8fe5662b3f14f0101b4a7ebd47c88d47ce9a0e
matveizhukov <motaigrok81@gmail.com>
2023-12-18 21:28:13 +0100

    zadanie dodatkowe 2 lab 4

--- Lab4.py
+++ Lab4.py
@@ -1,6 +1,3 @@
 zdanie = input("Podaj zdanie: ")
-zdanie = zdanie.lower().replace(" ", "")
-litery_w_kolejnosci = sorted(zdanie)
-print("Litery w kolejności alfabetycznej:", ", ".join(litery_w_kolejnosci))
-brakujace_litery = [litera for litera in "abcdefghijklmnopqrstuvwxyz" if litera not in zdanie]
-print("Litery, których brakuje:", ", ".join(brakujace_litery))
+zdanie_po_usunieciu = ''.join(zdanie[i] for i in range(len(zdanie)) if i % 2 == 0)
+print("Tekst po usunięciu znaków o nieparzystych indeksach:", zdanie_po_usunieciu)
