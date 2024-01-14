3e4943941aff5b55058168a12102e1ec3d695006
matveizhukov <motaigrok81@gmail.com>
2024-01-14 20:55:13 +0100

    zadanie 6 lab 5

--- Lab5.py
+++ Lab5.py
@@ -1,11 +1,11 @@
-def BMI(waga, wzrost):
-    bmi= waga/(wzrost*wzrost)
-    if bmi<=18.5:
-        print("Niedowaga, BMI=", bmi)
-    elif 18.5 <bmi < 24.9:
-        print("Norma, BMI=", bmi)
-    elif 25< bmi< 30:
-        print("Nadwaga, BMI=", bmi)
+def PoleTr(a, b, c):
+    if a<=0 or b<=0 or c<=0:
+        print("Podane dane są niepoprawne")
     else:
-        print("Otyłość, BMI=", bmi)
-BMI(90, 1.6)
+        if a+b<=c or b+c<=a or c+a<=b:
+            print("Z podanych danych nie uda się utworzyć trójkąt")
+        else:
+            p=(a+b+c)/2
+            pole=(p*(p-a)*(p-b)*(p-c))**0.5
+            print("Pole trójkąta to:", pole)
+PoleTr(a=2, b=4, c=5)
