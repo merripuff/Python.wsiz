97540b888addbc6e7bcb322dd9c74f40d53ba931
matveizhukov <motaigrok81@gmail.com>
2024-01-14 20:42:34 +0100

    zadanie 5 lab 5

--- Lab5.py
+++ Lab5.py
@@ -1,6 +1,11 @@
-def liczba(x):
-    if x<0:
-        print("Liczba x jest ujemna")
+def BMI(waga, wzrost):
+    bmi= waga/(wzrost*wzrost)
+    if bmi<=18.5:
+        print("Niedowaga, BMI=", bmi)
+    elif 18.5 <bmi < 24.9:
+        print("Norma, BMI=", bmi)
+    elif 25< bmi< 30:
+        print("Nadwaga, BMI=", bmi)
     else:
-        print( "Liczba x lest dodatnia")
-liczba(12)
+        print("Otyłość, BMI=", bmi)
+BMI(90, 1.6)
