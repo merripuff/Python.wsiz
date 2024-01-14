85fcb46a3c02f8e47284789274544c042d234d03
matveizhukov <motaigrok81@gmail.com>
2024-01-14 21:26:47 +0100

    zadanie 9 lab 5

--- Lab5.py
+++ Lab5.py
@@ -1,9 +1,12 @@
-def potega(a, n):
-    p = a**n
-    if n == 0:
-        print(1)
-    elif n < 0:
-        print(1/p)
+def fibonacci(n):
+    if n <= 0:
+        return "Błąd: n powinno być liczbą naturalną większą od zera."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
     else:
-        print(p)
-potega(a=2, n=-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
+numer_wyrazu = 12
+wynik = fibonacci(numer_wyrazu)
+print(f"{numer_wyrazu}-ty wyraz ciągu Fibonacciego: {wynik}")
