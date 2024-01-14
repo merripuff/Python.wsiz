1fa25e14f5aa476e2c35cb3b0ab1750860c42210
matveizhukov <motaigrok81@gmail.com>
2024-01-14 20:58:16 +0100

    zadanie 7 lab 5

--- Lab5.py
+++ Lab5.py
@@ -1,11 +1,5 @@
-def PoleTr(a, b, c):
-    if a<=0 or b<=0 or c<=0:
-        print("Podane dane są niepoprawne")
-    else:
-        if a+b<=c or b+c<=a or c+a<=b:
-            print("Z podanych danych nie uda się utworzyć trójkąt")
-        else:
-            p=(a+b+c)/2
-            pole=(p*(p-a)*(p-b)*(p-c))**0.5
-            print("Pole trójkąta to:", pole)
-PoleTr(a=2, b=4, c=5)
+def odwroc_string(tekst):
+    return tekst[::-1]
+przyklad_string = "Hello, World!"
+odwrocony_string = odwroc_string(przyklad_string)
+print(odwrocony_string)
