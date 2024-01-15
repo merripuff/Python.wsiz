af54609e1ad83e64f74113c42832ac9cc8ce946b
matveizhukov <motaigrok81@gmail.com>
2024-01-15 21:49:28 +0100

    zadania lab 4

--- Lab4.py
+++ Lab4.py
@@ -160,11 +160,11 @@
 
 
 # zad dodatk5
-# zdanie = input("Podaj zdanie: ")
-# slowa = zdanie.split()
-# najdluzsze_slowo = max(slowa, key=len)
-# print("Najdłuższe słowo:", najdluzsze_slowo)
-# print("Długość najdłuższego słowa:", len(najdluzsze_slowo))
+zdanie = input("Podaj zdanie: ")
+slowa = zdanie.split()
+najdluzsze_slowo = max(slowa, key=len)
+print("Najdłuższe słowo:", najdluzsze_slowo)
+print("Długość najdłuższego słowa:", len(najdluzsze_slowo))
 
 
 
